426071c1391cf0702b369ced5be2c3636d59e036
dtaylo56 <derek.taylor@optum.com>
2017-12-20 13:48:06 -0600

    Hello world example

---
+++ src/main/java/com/categories/collab/graphql/HelloWorld.java
@@ -0,0 +1,37 @@
+package com.categories.collab.graphql;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // schema in GraphQL is the central contract between the client and the server
+        // describes all the types of data and all the operations
+        String schema = "type Query{hello: String} schema{query: Query}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+        // Prints: {hello=world}
+    }
+}
